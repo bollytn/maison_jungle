9c5ebaf1b7f2519af0e09e522f764c449038edb3
bollytn <ps3online85@gmail.com>
2025-02-15 00:39:24 +0100

    euro to dtn

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -28,7 +28,7 @@ function Cart({ cart, updateCart }) {
 							</div>
 						))}
 					</ul>
-					<h3>Total :{total}€</h3>
+					<h3>Total :{total}dt</h3>
 					<button onClick={() => updateCart([])}>Vider le panier</button>
 				</div>
 			) : (
